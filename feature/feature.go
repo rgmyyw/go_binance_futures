97c01ff960642b697c0d072c7262dfa66a274300
rgmyyw <rgmyyw@users.noreply.github.com>
2026-09-12 12:16:14 +0800

    fix: 止损冷却标记改用 position.Side(平仓循环无开仓侧变量)

--- feature/feature.go
+++ feature/feature.go
@@ -221,7 +221,7 @@ func StartTrade(systemConfig *models.Config) {
 						// 数据库写入订单
 						insertCloseOrder(position, positionAmtFloatAbs, unRealizedProfit, position.MarkPrice, order.OrderID, systemConfig)
 						go CancelSymbolStopOrders(position.Symbol)
-						markLossCooldown(position.Symbol, positionSideLong)
+						markLossCooldown(position.Symbol, position.Side)
 
 						markPrice, _ := strconv.ParseFloat(position.MarkPrice, 64)
 						pusher.SetModuleName("futures").FuturesCloseOrder(notify.FuturesOrderParams{
@@ -257,7 +257,7 @@ func StartTrade(systemConfig *models.Config) {
 						// 数据库写入订单
 						insertCloseOrder(position, positionAmtFloatAbs, unRealizedProfit, position.MarkPrice, order.OrderID, systemConfig)
 						go CancelSymbolStopOrders(position.Symbol)
-						markLossCooldown(position.Symbol, positionSideShort)
+						markLossCooldown(position.Symbol, position.Side)
 
 						markPrice, _ := strconv.ParseFloat(position.MarkPrice, 64)
 						pusher.SetModuleName("futures").FuturesCloseOrder(notify.FuturesOrderParams{
